8770209d15f1e437b3c21c9e112f8a9e6490c239
Lynn Frank <fwebber@hashicorp.com>
2021-10-31 15:26:28 -0500

    Mute the console log for events

--- content.js
+++ content.js
@@ -5,7 +5,7 @@ rootDiv = document.getElementById("root")
 
 let playTableObserver = new MutationObserver(mutations => {
   for(let mutation of mutations) {
-    console.log(mutation);
+    // console.log(mutation);
 
     if (mutation.nextSibling && mutation.nextSibling.className === "absolute bottom-0 inset-x-0 p-4 flex justify-between items-center") {
       mutation.nextSibling.querySelector("button").click();
