3bd1bd17595c9938513e3734cf4149c7ec3ad3b7
Lynn Frank <fwebber@hashicorp.com>
2021-10-31 13:45:05 -0500

    Adds plugin code

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mute Spell Table
+
+This plugin finds and clicks the audio icon to mute the audio at the SpellTable login game lobby screen.
+
+## Setup
+
+1. Go to chrome://extensions/.
+2. At the top right, turn on Developer mode.
+3. Click Load unpacked.
+4. Select where you have downloaded/cloned this directory.
---
+++ content.js
@@ -0,0 +1,16 @@
+/* global browser */
+// console.log("muteSpellTable looking for play table!");
+
+rootDiv = document.getElementById("root")
+
+let playTableObserver = new MutationObserver(mutations => {
+  for(let mutation of mutations) {
+    console.log(mutation);
+
+    if (mutation.nextSibling && mutation.nextSibling.className === "absolute bottom-0 inset-x-0 p-4 flex justify-between items-center") {
+      mutation.nextSibling.querySelector("button").click();
+    }
+  }
+});
+
+playTableObserver.observe(rootDiv, { childList: true, subtree: true });
